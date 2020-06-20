2ec652f51a3f89f746fa27232aa191a7eb0b6294
Rocío Reina Delgado <rocioreinadlg@gmail.com>
2020-06-20 14:13:21 +0200

    segundo

--- src/main/java/com/politecnicomalaga/TerminoFibonaci.java
+++ src/main/java/com/politecnicomalaga/TerminoFibonaci.java
@@ -1,5 +1,12 @@
 package java.com.politecnicomalaga;
 
 public class TerminoFibonaci {
-
+	public int calcular(int termino){
+		int fib[] = new int[30];
+		fib[0] = 1;
+		fib[1] = 1;
+		for (int i = 2; i<=termino-1; i++)
+			fib[i] = fib[i-1] + fib[i-2];
+		return fib[termino-1];
+	}
 }
